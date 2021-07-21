caba0ea023c7109bd28aaa6405e26a429a8ef8c9
JustXKH <1414413917@qq.com>
2021-07-21 19:41:59 +0800

    modify error

--- Video_2_Pictures.py
+++ Video_2_Pictures.py
@@ -1,8 +1,8 @@
 import cv2
 import os
 
-os.chdir( r'C:\Users\xkh\Desktop\sample\img')
-filename = r'C:\Users\xkh\Desktop\sample\WIN_20210416_14_30_37_Pro.mp4'
+os.chdir( r'C:\Users\xkh\Desktop\20210721_162126\img')
+filename = r'C:\Users\xkh\Desktop\20210721_162126\20210721_162126_VIS_H264.MOV'
 
 videoCapture1 = cv2.VideoCapture(filename)
 
@@ -10,11 +10,10 @@ status, frame = videoCapture1.read()
 index = 0
 while True:
     status, frame = videoCapture1.read()
-    cv2.imwrite('img_'+str(index)+'.jpg', frame)
-    index += 1
-
     if not status:
         print('video is all read')
         break
-    
-    
+
+    cv2.imwrite('img_'+str(index)+'.jpg', frame)
+    index += 1
+
